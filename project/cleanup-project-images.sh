d2ca19ec268de98cfaa2f37bdbe1739b03c74dd3
msgarbossa <mikeysky@gmail.com>
2023-11-02 20:51:30 -0700

    cleanup images

---
+++ project/cleanup-project-images.sh
@@ -0,0 +1,8 @@
+
+docker image rm project_authentication-service:latest
+docker image rm project_mailer-service:latest
+docker image rm project_front-end:latest
+docker image rm project_micro-caddy:latest
+docker image rm project_logger-service:latest
+docker image rm project_broker-service:latest
+docker image rm project_listener-service:latest
